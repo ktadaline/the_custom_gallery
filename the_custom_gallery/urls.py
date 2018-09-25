9323e2f1e84e179f8dbd64b4cd265b9d5ae85546
Katie Adaline Sroub <ksroub@gmail.com>
2018-09-25 12:57:44 -0400

    More React Setup

--- the_custom_gallery/urls.py
+++ the_custom_gallery/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('artist_profile.urls')),
+    path('', include('frontend.urls')),
 ]
