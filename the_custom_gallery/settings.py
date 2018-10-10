42e917ca2bfa68b6ada3adbc471354ac09233a4b
Katie Adaline Sroub <ksroub@gmail.com>
2018-10-10 12:54:44 -0400

    configure settings for jwt tokens

--- the_custom_gallery/settings.py
+++ the_custom_gallery/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,7 +41,10 @@ INSTALLED_APPS = [
     'artist_profile',
     'rest_framework',
     'frontend',
-    #'rest_framework.authtoken'
+    'rest_auth',
+    'django.contrib.sites',
+    'allauth.account',
+    'rest_auth.registration',
 ]
 
 MIDDLEWARE = [
@@ -83,6 +87,26 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# Configure the JWTs to expire after 1 hour, and allow users to refresh near-expiration tokens
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=1),
+    'JWT_ALLOW_REFRESH': True,
+}
+
+# Make JWT Auth the default authentication mechanism for Django
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+}
+
+# Enables django-rest-auth to use JWT tokens instead of regular tokens.
+REST_USE_JWT = True
+
+#requirement for registration app to work due to django.contrib.sites dependency
+
+SITE_ID = 1
 
 
 # Password validation
--- the_custom_gallery/urls.py
+++ the_custom_gallery/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from artist_profile import views
+from rest_framework_jwt.views import refresh_jwt_token
 
 #wire up ArtistProfileListCreate and ArtListCreate to api/artist/ and api/art/
 
@@ -27,6 +28,9 @@ urlpatterns = [
     path('', include('frontend.urls')),
     path('artists_/', views.Artist_List.as_view(), name='artists_'),
     path('art_/', views.Art_List.as_view(), name='art_'),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('refresh-token/', refresh_jwt_token),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
